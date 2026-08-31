c2a6415a31072e5fbdd3e364a8e8a159337cc69a
kptmail-game <kptmail@gmail.com>
2026-08-31 10:18:48 -0500

    Create blank UE5 C++ project SNHU_312_Survival on engine 5.7.4

---
+++ SNHU_312_Survival/Source/SNHU_312_Survival/SNHU_312_Survival.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SNHU_312_Survival.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SNHU_312_Survival, "SNHU_312_Survival" );
